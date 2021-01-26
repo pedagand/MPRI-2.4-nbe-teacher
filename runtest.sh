6d2efb2d3d9ea72341c08a8e5845eef4d6e3a1d8
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2021-01-26 16:04:01 +0100

    Fix test runner

--- runtest.sh
+++ runtest.sh
@@ -5,9 +5,9 @@ FILENAME=$1
 dune runtest &>/dev/null
 
 echo "Selectively run tests for $FILENAME"
-./_build/default/src/.src.inline-tests/inline_test_runner_src.exe \
+./_build/default/src/.nbe.inline-tests/inline_test_runner_nbe.exe \
     inline-test-runner                                            \
-    src                                                           \
+    nbe                                                           \
     -source-tree-root ..                                          \
     -only-test $1
 if [ $? -eq 0 ]; then
